8058b7b8d73a5c74fc59ae167fc68d32eb425973
Tom Dietz <tom@ottowarp.com>
2018-06-20 21:09:07 -0500

    Logging

--- src/main/java/com/amazonaws/serverless/tweetsentiment/TweetSentiment.java
+++ src/main/java/com/amazonaws/serverless/tweetsentiment/TweetSentiment.java
@@ -1,11 +1,16 @@
 package com.amazonaws.serverless.tweetsentiment;
+import lombok.extern.slf4j.Slf4j;
+
 import java.util.List;
 
 /**
  * Core Tweet Sentiment logic.
  */
+@Slf4j
 public class TweetSentiment {
-    public void publishSentimentMetrics(List<String> tweetsStrings) {
-        //TODO
+
+    public void publishSentimentMetrics(List<String> tweetStrings) {
+
+        log.info("Recieved tweets:{}", tweetStrings);
     }
 }
